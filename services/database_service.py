ecaebcacbb033cb49be44d4c526b487aaaf087d2
Jake West <jacob.west@ons.gov.uk>
2023-11-15 11:22:32 +0000

    Changed to use session not connection

--- services/database_service.py
+++ services/database_service.py
@@ -1,8 +1,9 @@
 import pymysql
 import sqlalchemy
-from google.cloud.sql.connector import Connector
-from sqlalchemy import MetaData, Table, text, insert
 
+from google.cloud.sql.connector import Connector
+from sqlalchemy import MetaData, Table, text, insert, Engine
+from sqlalchemy.orm import Session
 from models.database_connection_model import DatabaseConnectionModel
 
 
@@ -17,18 +18,20 @@ class DatabaseService:
         source_table = self.__get_table(source_database, table_name)
         destination_table = self.__get_table(destination_database, table_name)
 
+        destination_database_session = Session(destination_database)
+
         with source_database.connect() as source_connection:
             table_data = source_connection.execute(source_table.select())
 
-            with destination_database.connect() as dest_connection:
-                dest_connection.execute(text("truncate " + table_name))
+            with destination_database_session.begin():
+                destination_database_session.execute(text("truncate " + table_name))
                 for row in table_data:
                     insert_statement = insert(destination_table).values(row)
-                    dest_connection.execute(insert_statement)
+                    destination_database_session.execute(insert_statement)
 
-                dest_connection.commit()
 
-    def __get_database(self, instance_name: str) -> sqlalchemy.engine.base.Engine:
+
+    def __get_database(self, instance_name: str) -> Engine:
         connection = self.__get_connection(instance_name, self._connection_model)
 
         return sqlalchemy.create_engine(
@@ -50,7 +53,7 @@ class DatabaseService:
 
         return connector.connect(
             instance_connection_string=instance_name,
-            driver=sql_connection.database_driver,
+            driver=sql_connection.database_driver,  # pymysql
             user=sql_connection.database_username,
             password=sql_connection.database_password,
             db=sql_connection.database_name,
